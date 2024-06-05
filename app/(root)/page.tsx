c974ffb3b0ca98962fb48a48c79264bcfdbc096a
57Ajay <57ajay.u@gmail.com>
2024-06-05 22:37:28 +0530

    minor

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@/components/ui/button";
+import { podcastData } from "@/constants";
+import PodcastCard from "@/components/PodcastCard";
 
 const Home = () => {
   return (
@@ -8,7 +10,9 @@ const Home = () => {
           Trending Podcast
         </h1>
         <Button className="text-white-1 bg-orange-1">Button</Button>
-        
+        {podcastData.map(({imgURL, title, id, description})=>(
+          <PodcastCard key={id} imgUrl={imgURL} title={title} description={description} podcastId={id}/>
+        ))}
       </section>
     </div>
   );
---
+++ components/PodcastCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PodcastCard = ({imgUrl, title, podcastId, description}: {imgUrl: string, title: string, description: string, podcastId: number}) => {
+  return (
+    <div className='curser-pointer hover:shadow-2xl hover:shadow-orange-1'></div>
+  )
+}
+
+export default PodcastCard; 
