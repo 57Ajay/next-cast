19dddec4c0361841fc1d255dd57e7c7e19b2086e
57Ajay <57ajay.u@gmail.com>
2024-06-06 18:21:50 +0530

    integrating convex for backend

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,31 @@
-import { Button } from "@/components/ui/button";
+"use client";
 import { podcastData } from "@/constants";
 import PodcastCard from "@/components/PodcastCard";
 
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+
+
+
+
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-950">
       <section className="mt-9 flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">
           Trending Podcast
         </h1>
-        <Button className="text-white-1 bg-orange-1">Button</Button>
-        {podcastData.map(({imgURL, title, id, description})=>(
-          <PodcastCard key={id} imgUrl={imgURL} title={title} description={description} podcastId={id}/>
-        ))}
+
+        <div className="flex min-h-screen flex-col items-center justify-between p-24 text-white-1">
+        {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+      </div>
+  
+        <div className="podcast_grid">
+          {podcastData.map(({imgURL, title, id, description})=>(
+            <PodcastCard key={id} imgUrl={imgURL} title={title} description={description} id={id}/>
+          ))}
+        </div>
       </section>
     </div>
   );
--- components/PodcastCard.tsx
+++ components/PodcastCard.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
-
-const PodcastCard = ({imgUrl, title, podcastId, description}: {imgUrl: string, title: string, description: string, podcastId: number}) => {
+import Image from 'next/image';
+const PodcastCard = ({imgUrl, title, id, description}: {imgUrl: string, title: string, description: string, id: number}) => {
   return (
-    <div className='curser-pointer hover:shadow-2xl hover:shadow-orange-1'></div>
+    <div className='cursor-pointer'>
+      <figure className='flex flex-col gap-2'>
+        <Image src={imgUrl} alt={title} width={175} height={175} className='aspect-square h-fit w-full rounded-xl 2xl:size-[200px]' />
+        <div className='flex flex-col gap-1'>
+          <h1 className='text-16 truncate font-bold text-white-1'>{title}</h1>
+          <h2 className='text-12 truncate font-normal capitalize text-white-4'>{description}</h2>
+        </div>
+      </figure>
+    </div>
   )
 }
 
